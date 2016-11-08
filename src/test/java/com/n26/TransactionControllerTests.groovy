84accff2bc35f40deaeb773a072827689a32532c
Biniam Asnake <biniam.asnake@kaufda.de>
2016-11-08 11:13:33 +0100

    Removed unncessary plugins

--- src/test/java/com/n26/TransactionControllerTests.groovy
+++ src/test/java/com/n26/TransactionControllerTests.groovy
@@ -1,13 +1,10 @@
 package com.n26
-
 import com.n26.controller.TransactionController
 import com.n26.model.Transaction
-import com.n26.service.TransactionService
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
@@ -29,9 +26,6 @@ public class TransactionControllerTests {
 
     @Autowired
     private MockMvc mvc
-
-    @MockBean
-    private TransactionService transactionService
 
     @Test
     public void shouldCreateTransactionWithoutParentId() throws Exception {
